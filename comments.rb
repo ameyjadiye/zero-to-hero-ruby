65e8ab6824d5444195c7bae7a15932cf0d119dab
Amey Jadiye <ameyjadiye@gmail.com>
2014-09-17 23:50:24 +0530

    Cool commenets in .rb

---
+++ comments.rb
@@ -0,0 +1,11 @@
+#this is full line comment
+
+puts "This is comment too ==>" #Comment
+
+
+puts "Multi line comment like java use /* write whatever shit you want */"
+
+=begin
+This is multiline commnent.
+This dont need any kind of # symbol
+=end
