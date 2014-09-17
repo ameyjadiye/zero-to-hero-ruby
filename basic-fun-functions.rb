eba357caffbd13bb6db401c1549d8e93ae6383a0
Amey Jadiye <ameyjadiye@gmail.com>
2014-09-17 23:50:04 +0530

    Few basic functions

---
+++ basic-fun-functions.rb
@@ -0,0 +1,15 @@
+#counting length
+puts "Hi, this is amey :)".length
+
+#Reverseing string.
+
+puts "amey jadiye"
+puts "amey jadiye".reverse
+
+# upper and lower case
+
+puts "tHiS iS wEiRd StRiNg"
+puts "tHiS iS wEiRd StRiNg".upcase
+puts "tHiS iS wEiRd StRiNg".downcase
+
+
